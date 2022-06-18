114d448264838a445b59de5a0a42724b81c71970
anaWilliams123 <anitarcoiris@gmail.com>
2022-06-18 11:43:35 -0400

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,35 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function menorMayor(numeros) {
+  // La función llamada 'menorMayor' recibe como argumento un arreglo de números llamado 'numeros' y debe devolver un
+  // arreglo que contenga el menor número del arreglo 'numeros' en la posición cero y el mayor número del arreglo
+  // 'numeros' en la posición 1.
+  // Ej:
+  // menorMayor([4, 6, 1, 7, 15]) debe retornar [1, 15]
+  // ya que 1 es el número más chico (menor) dentro del arreglo [4, 6, 1, 7, 15]
+  // y 15 es el número más grande (mayor) dentro del arreglo [4, 6, 1, 7, 15]
+
+  // Tu código aca:
+var mayor = numeros[0]; 
+var menor = numeros[0];
+  
+  for (var i = 1; i< numeros.length; i++){ 
+    if (numeros[i] > mayor){
+      mayor = numeros[i]; 
+    } else if (numeros[i] < menor){
+      menor = numeros[i]; 
+    }
+  }
+  var array= [menor,mayor]
+  return array;
+}
+  
+
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = menorMayor
